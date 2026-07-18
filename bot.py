b2a08cc09441a27b0d51910c96140bcd263d00c2
Helga ₿lue 癸亥 <111192958+april19833@users.noreply.github.com>
2026-07-18 13:12:04 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,19 @@
+from telegram import Update
+from telegram.ext import Application, MessageHandler, ContextTypes, filters
+
+TOKEN = "8343671858:AAEFHlF8lgT6q_oOr2rVCs9MVNhJbhVCM90"
+ADMIN_ID = 379198558
+
+async def forward_to_admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.forward_message(
+        chat_id=ADMIN_ID,
+        from_chat_id=update.effective_chat.id,
+        message_id=update.message.message_id,
+    )
+
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.PHOTO, forward_to_admin))
+app.add_handler(MessageHandler(filters.Document.IMAGE, forward_to_admin))
+
+app.run_polling()
